9ce7669c0c3a981624092a5e988e2ca218434c17
poluektova <a.poluektova88@gmail.com>
2019-10-19 09:24:28 +0400

    Правки по позиционированию карты и отступам

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
 var button = document.querySelector(".hotel-button");
 var modal = document.querySelector(".search-form");
+var checkin = modal.querySelector("[name=checkin]");
+var checkout = modal.querySelector("[name=checkout]");
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.toggle("modal-show");
+checkin.focus();
+modal.addEventListener("submit", function(evt){
+  if (!checkin.value && !checkout.value)
+  evt.preventDefault();
+  console.log("Нужно ввести дату заезда и выезда");
+})
 });
 
 window.addEventListener("keydown", function (evt) {
