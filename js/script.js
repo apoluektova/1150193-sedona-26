ef787fc8dd523b40fa55388d6d94d19f790be836
poluektova <a.poluektova88@gmail.com>
2019-10-22 17:20:58 +0400

    Добавлена анимация и проверка формы

--- js/script.js
+++ js/script.js
@@ -2,15 +2,44 @@ var button = document.querySelector(".hotel-button");
 var modal = document.querySelector(".search-form");
 var checkin = modal.querySelector("[name=checkin]");
 var checkout = modal.querySelector("[name=checkout]");
+var adults = modal.querySelector("[name=adults]");
+var kids = modal.querySelector("[name=kids]");
+var isStorageSupport = true;
+var storageAdults = "";
+var storageKids = "";
+
+try {
+  storageAdults = localStorage.getItem("adults");
+  storageKids = localStorage.getItem("kids");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (storageAdults || storageKids) {
+  adults.value = storageAdults;
+  kids.value = storageKids;
+}
+
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   modal.classList.toggle("modal-show");
-checkin.focus();
-modal.addEventListener("submit", function(evt){
-  if (!checkin.value && !checkout.value)
+  modal.classList.toggle("modal-hide");
+  checkin.focus();
+});
+
+
+modal.addEventListener("submit", function(evt) {
+  if (!checkin.value || !checkout.value || !adults.value || !kids.value) {
   evt.preventDefault();
-  console.log("Нужно ввести дату заезда и выезда");
-})
+  modal.classList.remove("modal-error");
+  modal.offsetWidth = modal.offsetWidth;
+  modal.classList.add("modal-error");
+} else {
+  if (isStorageSupport) {
+  localStorage.setItem("adults", adults.value);
+  localStorage.setItem("kids", kids.value);
+  }
+}
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -18,6 +47,7 @@ window.addEventListener("keydown", function (evt) {
       evt.preventDefault();
       if (modal.classList.contains("modal-show")) {
         modal.classList.remove("modal-show");
+        modal.classList.remove("modal-error");
       }
     }
   });
