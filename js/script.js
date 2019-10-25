f409c326d99f24cc17afd2ebaf25d125b19a35bb
poluektova <a.poluektova88@gmail.com>
2019-10-26 00:24:12 +0400

    Правки по критериям

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var button = document.querySelector(".hotel-button");
 var modal = document.querySelector(".search-form");
+var form = modal.querySelector(".popup");
 var checkin = modal.querySelector("[name=checkin]");
 var checkout = modal.querySelector("[name=checkout]");
 var adults = modal.querySelector("[name=adults]");
@@ -22,18 +23,20 @@ if (storageAdults || storageKids) {
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
+  modal.classList.remove("modal-error");
   modal.classList.toggle("modal-show");
   modal.classList.toggle("modal-hide");
   checkin.focus();
 });
 
+modal.classList.add("modal-hide");
 
 modal.addEventListener("submit", function(evt) {
-  if (!checkin.value || !checkout.value || !adults.value || !kids.value) {
+  if (!checkin.value || !checkout.value || !adults.value || !kids.value || adults.value == "0") {
   evt.preventDefault();
-  modal.classList.remove("modal-error");
-  modal.offsetWidth = modal.offsetWidth;
-  modal.classList.add("modal-error");
+  form.classList.remove("modal-error");
+  form.offsetWidth = form.offsetWidth;
+  form.classList.add("modal-error");
 } else {
   if (isStorageSupport) {
   localStorage.setItem("adults", adults.value);
@@ -47,7 +50,8 @@ window.addEventListener("keydown", function (evt) {
       evt.preventDefault();
       if (modal.classList.contains("modal-show")) {
         modal.classList.remove("modal-show");
-        modal.classList.remove("modal-error");
+        form.classList.remove("modal-error");
+        modal.classList.add("modal-hide");
       }
     }
   });
